39a4eebeebafaef65453a267784b2f418799f504
eakinloose <eakinloose@gmail.com>
2022-09-27 23:19:41 +0100

    chat UI and pagination

--- src/pages/Homepage/Homepage.js
+++ src/pages/Homepage/Homepage.js
@@ -1,29 +1,35 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { logIn } from "../../features/auth/auth";
 import { sendMessage, updateMessages } from "../../features/messages/messages";
+import { Wrapper } from "./HomepageStyles";
+import avatar from "../../images/avatar.png";
 
 const Homepage = () => {
    const loggedUser = useSelector((state) => state.user.loggedUser);
    const messages = useSelector((state) => state.messages.messages);
    const dispatch = useDispatch();
+   const chatRef = useRef();
    const [message, setMessage] = useState("");
+   const [paginatedData, setPaginatedData] = useState([]);
+   const [chatSize, setChatSize] = useState(25);
 
    useEffect(() => {
       dispatch(logIn());
-      return;
    }, [dispatch]);
 
-   const sendMessageHandler = (e) => {
-      e.preventDefault();
-      dispatch(
-         sendMessage({
-            author: loggedUser,
-            text: message,
-         })
-      );
-      setMessage("");
-   };
+   useEffect(() => {
+      const getPaginetedData = () => {
+         let slicedmsg = messages.slice(
+            messages.length - chatSize,
+            messages.length
+         );
+         setPaginatedData(slicedmsg);
+      };
+
+      getPaginetedData();
+   }, [chatSize, message, messages]);
+
    useEffect(() => {
       const onUpdateMessage = () => {
          setTimeout(() => dispatch(updateMessages()), 0);
@@ -31,38 +37,86 @@ const Homepage = () => {
       window.addEventListener("storage", onUpdateMessage);
       return () => window.removeEventListener("storage", onUpdateMessage);
    }, [dispatch]);
-   return (
-      <>
-         {messages ? (
-            <>
-               {messages.map((msg, index) => (
-                  <h2 key={index}>{msg.text}</h2>
-               ))}
-            </>
-         ) : null}
 
-         <br />
-         <br />
-         <br />
-         <div>{loggedUser} is logged in on this page</div>
-         <br />
-         <br />
-         <br />
-         <div>
-            <form onSubmit={sendMessageHandler}>
-               {loggedUser ? (
-                  <label htmlFor="message">{loggedUser}</label>
-               ) : (
-                  <label htmlFor="message">Anonymous</label>
-               )}
-               <textarea
-                  id="message"
-                  onChange={(e) => setMessage(e.target.value)}
-               />
-               <button>send</button>
-            </form>
+   const sendMessageHandler = (e) => {
+      if (message.length < 1) {
+         return;
+      } else {
+         e.preventDefault();
+         dispatch(
+            sendMessage({
+               author: loggedUser,
+               text: message,
+            })
+         );
+      }
+      setMessage("");
+   };
+
+   const loadmoreMessages = () => {
+      if (messages.length < chatSize + 25) {
+         setChatSize(messages.length);
+      } else {
+         setChatSize(chatSize + 25);
+      }
+   };
+
+   useEffect(() => {
+      const scrollToMyRef = () => {
+         const scroll =
+            chatRef.current.scrollHeight - chatRef.current.clientHeight;
+         chatRef.current.scrollTo(0, scroll);
+      };
+
+      scrollToMyRef();
+   }, [message]);
+
+   return (
+      <Wrapper>
+         <div className="header">
+            <h3>welcome {loggedUser}</h3>
+            <hr />
          </div>
-      </>
+
+         <div className="msgWrapper" ref={chatRef}>
+            <div className="loadmore">
+               {messages.length !== chatSize ? (
+                  <button onClick={loadmoreMessages}>load more messages</button>
+               ) : null}
+            </div>
+
+            {messages ? (
+               <>
+                  {paginatedData.map((msg, index) => (
+                     <div
+                        key={index}
+                        className={
+                           msg.author === loggedUser ? "left" : "message"
+                        }
+                     >
+                        <div className="circle">
+                           <img src={avatar} alt="avatar" />
+                        </div>
+                        <div className="content">
+                           <p className="sender">{msg.author}</p>
+                           <p>{msg.text}</p>
+                        </div>
+                     </div>
+                  ))}
+               </>
+            ) : null}
+         </div>
+
+         <form onSubmit={sendMessageHandler}>
+            <textarea
+               id="message"
+               placeholder="Message..."
+               onChange={(e) => setMessage(e.target.value)}
+               value={message}
+            />
+            <button>send</button>
+         </form>
+      </Wrapper>
    );
 };
 
--- src/pages/Homepage/HomepageStyles.js
+++ src/pages/Homepage/HomepageStyles.js
@@ -1,35 +1,128 @@
 import styled from "styled-components";
 
 export const Wrapper = styled.div`
-   width: 90%;
-   max-width: 500px;
-   margin: 9rem auto;
+   width: 50%;
+   max-width: 450px;
+   min-width: 365px;
+   margin: 5vh auto;
+   border-radius: 10px;
+   height: 90vh;
+   background: white;
+   display: flex;
+   flex-direction: column;
+   justify-content: space-between;
+   padding: 2rem 1rem 1rem;
+   box-shadow: rgba(99, 99, 99, 0.2) 0px 2px 8px 0px;
 
-      form {
-         display: flex;
-         flex-direction: column;
-         justify-content: center;
-         align-content: center;
-         padding: 0.7rem 0;
+   .header {
+      text-align: center;
+
+      h3 {
+         margin: 10px;
+         text-transform: uppercase;
+         font-weight: 400;
+      }
+   }
+
+   .msgWrapper {
+      height: 82%;
+      overflow-y: scroll;
+      position: relative;
+
+      .loadmore {
+         width: 100%;
+         text-align: center;
+
+         button {
+            margin: 1rem;
+            padding: 3px 5px;
+            border-radius: 5px;
+            font-size: 13px;
+            outline: none;
+         }
       }
 
+      .message,
+      .left {
+         width: 80%;
+         background: whitesmoke;
+         border-radius: 10px;
+         padding: 10px;
+         margin: 10px 0;
+         display: flex;
+
+         .circle {
+            height: 40px;
+            width: 40px;
+            border-radius: 50%;
+            background: #249edd;
+            margin-right: 1rem;
+            margin-left: 0;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+
+            img {
+               width: 65%;
+               height: 65%;
+            }
+         }
+
+         .sender {
+            text-transform: capitalize;
+            font-weight: 400;
+            margin-bottom: 5px;
+         }
+
+         .content {
+            width: 80%;
+         }
+      }
+
+      .left {
+         background: teal;
+         background: #3f75cc;
+         background: #249edd;
+         color: whitesmoke;
+         margin-left: 20%;
+         display: flex;
+         flex-direction: row-reverse;
+
+         .circle {
+            background: whitesmoke;
+            margin-right: 0;
+            margin-left: 1rem;
+         }
+      }
+   }
+
+   form {
+      width: 100%;
+      background-color: white;
+      display: flex;
+      flex-direction: column;
+
       textarea {
-         margin: 0.7rem 0;
-         outline: none;
+         margin: 7px 0;
+         height: 50px;
+         padding: 10px;
       }
 
       textarea:focus,
-      textarea:active,
-      textarea:hover {
+      textarea:active {
          outline: none;
-         margin: 0.7rem 0;
-         /* width: 100%; */
       }
 
       button {
-         padding: 1rem;
+         padding: 0.8rem;
+         background: #249edd;
          color: white;
          border: none;
-         background: teal;
+         cursor: pointer;
       }
+
+      button:hover {
+         filter: brightness(80%);
+      }
+   }
 `;
